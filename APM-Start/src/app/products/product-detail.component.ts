4dba70c62e77bd0f79a97fb14a5f41ae719a5a82
James Skemp <strivinglife@gmail.com>
2021-08-22 12:52:21 -0500

    Add start of routing to product detail page

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    // Get the id one time.
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    // If the route may change
+    /*this.route.paramMap.subscribe(
+      params => console.log(params.get('id'))
+    )*/
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
